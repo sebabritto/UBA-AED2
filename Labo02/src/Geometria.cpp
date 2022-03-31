987ebdf63182b4ef343cf0cf420ebae764ca5ab2
Sebastian Britto <81824422+sebabritto@users.noreply.github.com>
2022-03-31 10:23:39 -0300

    Add files via upload

---
+++ Labo02/src/Geometria.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+using uint = unsigned int;
+
+// Ejercicio 1
+
+class Rectangulo {
+    public:
+        Rectangulo(uint alto, uint ancho);
+        uint alto();
+        uint ancho();
+        float area();
+
+    private:
+        int alto_;
+        int ancho_;
+
+};
+
+Rectangulo::Rectangulo(uint alto, uint ancho) : /* Completar */ {};
+
+uint Rectangulo::alto() {
+    // Completar
+}
+
+// Completar definición: ancho
+
+// Completar definición: area
+
+// Ejercicio 2
+
+// Clase Elipse
+
+// Ejercicio 3
+
+class Cuadrado {
+    public:
+        Cuadrado(uint lado);
+        uint lado();
+        float area();
+
+    private:
+        Rectangulo r_;
+};
+
+Cuadrado::Cuadrado(uint lado): r_(lado, lado) {}
+
+uint Cuadrado::lado() {
+    // Completar
+}
+
+float Cuadrado::area() {
+    // Completar
+}
+
+// Ejercicio 4
+
+// Clase Circulo
+
+
+// Ejercicio 5
+
+ostream& operator<<(ostream& os, Rectangulo r) {
+    os << "Rect(" << r.ancho() << ", " << r.alto() << ")";
+    return os;
+}
+
+// ostream Elipse
+
+// Ejercicio 6
+
