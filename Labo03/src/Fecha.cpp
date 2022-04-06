44ae17b7fc723af70067f7df2965891af660e03f
Sebastian Britto <81824422+sebabritto@users.noreply.github.com>
2022-04-06 11:10:59 -0300

    Add files via upload

--- Labo03/src/Fecha.cpp
+++ Labo03/src/Fecha.cpp
@@ -1,52 +1,7 @@
-// Typedef
-typedef int Anio;
-typedef int Mes;
-typedef int Dia;
+#include "Periodo.h"
+#include "Meses.h"
+#include "Funciones.h"
 
-// MESES
-const Mes ENERO = 1;
-const Mes FEBRERO = 2;
-const Mes MARZO = 3;
-const Mes ABRIL = 4;
-const Mes MAYO = 5;
-const Mes JUNIO = 6;
-const Mes JULIO = 7;
-const Mes AGOSTO = 8;
-const Mes SEPTIEMBRE = 9;
-const Mes OCTUBRE = 10;
-const Mes NOVIEMBRE = 11;
-const Mes DICIEMBRE = 12;
-
-bool esBisiesto(Anio anio) {
-  bool p = anio % 4 == 0;
-  bool q = anio % 100 == 0;
-  bool r = anio % 400 == 0;
-  return p and ((not q) or r);
-}
-
-int diasEnMes(int anio, int mes) {
-  if (mes == FEBRERO) {
-    if (esBisiesto(anio)) {
-      return 29;
-    } else {
-      return 28;
-    }
-  }
-  // No es febrero
-  if (mes < AGOSTO) {
-    if (mes % 2 == ENERO % 2) {
-      return 31;
-    } else {
-      return 30;
-    }
-  } else {
-    if (mes % 2 == AGOSTO % 2) {
-      return 31;
-    } else {
-      return 30;
-    }
-  }
-}
 
 class Periodo;
 
@@ -105,35 +60,6 @@ bool Fecha::operator<(Fecha o) const {
 
 bool operator!=(Fecha f1, Fecha f2) {
   return not (f1 == f2);
-}
-
-class Periodo {
- public:
-  Periodo(int anios, int meses, int dias);
-
-  int anios() const;
-  int meses() const;
-  int dias() const;
-
- private:
-  int anios_;
-  int meses_;
-  int dias_;
-};
-
-Periodo::Periodo(int anios, int meses, int dias) : anios_(anios), 
-    meses_(meses), dias_(dias) {};
-
-int Periodo::anios() const {
-  return anios_;
-}
-
-int Periodo::meses() const {
-  return meses_;
-}
-
-int Periodo::dias() const {
-  return dias_;
 }
 
 void Fecha::ajustar_fecha() {
