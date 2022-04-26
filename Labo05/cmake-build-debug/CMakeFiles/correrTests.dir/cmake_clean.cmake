213d23a8aa3b3953491b6ee539eae1c4615020b7
Sebastian Britto <81824422+sebabritto@users.noreply.github.com>
2022-04-26 18:00:00 -0300

    Add files via upload

---
+++ Labo05/cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrTests.dir/tests/lista_tests.cpp.o"
+  "correrTests"
+  "correrTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Labo05/cmake-build-debug/CMakeFiles/correrValgrind.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrValgrind"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/correrValgrind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
