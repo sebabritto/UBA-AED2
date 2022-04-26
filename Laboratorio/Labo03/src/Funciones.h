5dfb5f660ea7c9fbb2fd5981b388baa07ed4dadb
Sebastian Britto <81824422+sebabritto@users.noreply.github.com>
2022-04-26 18:10:27 -0300

    Add files via upload

---
+++ Laboratorio/Labo03/src/Funciones.h
@@ -0,0 +1,9 @@
+#include "Meses.h"
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+bool esBisiesto(Anio anio);
+
+int diasEnMes(int anio, int mes);
+
+#endif //FUNCIONES_H
---
+++ Laboratorio/Labo03/src/Periodo.cpp
@@ -0,0 +1,17 @@
+/* Completar */
+#include "Periodo.h"
+
+Periodo::Periodo(int anios, int meses, int dias) : anios_(anios),
+                                                   meses_(meses), dias_(dias) {};
+
+int Periodo::anios() const {
+    return anios_;
+}
+
+int Periodo::meses() const {
+    return meses_;
+}
+
+int Periodo::dias() const {
+    return dias_;
+}
