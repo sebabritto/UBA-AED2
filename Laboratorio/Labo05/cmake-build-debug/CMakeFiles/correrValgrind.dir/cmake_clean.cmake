85df213ec99e0ada3c06d165d7ee8b3687abbb6d
seb4s <seb4sbritto@gmail.com>
2022-05-04 17:07:06 -0300

    .

--- Laboratorio/Labo05/cmake-build-debug/CMakeFiles/correrValgrind.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/correrValgrind"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/correrValgrind.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
